bf2d5613b26688508097bf501790b04be89e365b
Dalton Luis Boer <dluisbr@yahoo.com.br>
2019-05-04 12:12:16 -0300

    model category

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,14 +1,15 @@
 import { InMemoryDbService } from "angular-in-memory-web-api";
-import { CategoriesModule } from './pages/categories/categories.module';
+import { Category } from "./pages/categories/shared/category.model";
 
 export class InMemoryDatabase implements InMemoryDbService {
     createDb() {
-        const categories = [
+        const categories: Category[] = [
             { id: 1, name: "Moradia", description: "Pagamentos de contas da casa" },
             { id: 2, name: "Saúde", description: "Planos de saúde e remédios" },
             { id: 3, name: "Lazer", description: "Cinema, parques, praia, etc..." },
             { id: 4, name: "Salário", description: "Recebimento de salário" },
-            { id: 5, name: "Freelas", description: "Trabalhos como freelancer" }
+            { id: 5, name: "Freelas", description: "Trabalhos como freelancer" },
+            { id: 6, name: "Transporte", description: "Metro, onibus, aplicativo, ..."}
         ];
 
         return { categories }
