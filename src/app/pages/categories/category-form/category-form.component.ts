0b4872040fb06fe4cc7b86a7879067c9942c93d1
dluisbrbr <dluisbr@yahoo.com.br>
2019-05-08 14:14:35 -0300

    category form complete

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -42,6 +42,15 @@ export class CategoryFormComponent implements OnInit {
     this.setPageTitle();
   }
 
+  submitForm(){
+    this.submittingForm = true;
+    if ( this.currentAction == 'new' ){
+      this.createCategory();
+    }else {
+      this.updateCategory();
+    }
+  }
+
   //PRIVATE METHODS
   private setCurrentAction(){
     if ( this.route.snapshot.url[0].path == 'new' )
@@ -79,6 +88,45 @@ export class CategoryFormComponent implements OnInit {
     else{
       const categoryName = this.category.name || '';
       this.pageTitle = 'Cadastro de novas categoria: ' + categoryName;
+    }
+  }
+
+  private createCategory(){
+    const category: Category = Object.assign( new Category(), this.categoryForm.value );
+    this.categoryService.create(category)
+      .subscribe(
+        category => this.actionsForSuccess(category),
+        error => this.actionsForError(error)
+      )
+  }
+
+  private updateCategory(){
+    const category: Category = Object.assign( new Category(), this.categoryForm.value );
+    this.categoryService.update(category)
+      .subscribe(
+        category => this.actionsForSuccess(category),
+        error => this.actionsForError(error)
+      )
+  }
+
+  private actionsForSuccess(category: Category){
+    toastr.success('Solicitação processada com sucesso!');
+
+    // redirect /reload component page
+    this.router.navigateByUrl('categories', {skipLocationChange: true}).then(
+      () => this.router.navigate(['categories', category.id, 'edit'])
+    )
+  }
+
+  private actionsForError(error){
+    toastr.error('Ocorreu um erro ao processar a sua solicitação');
+
+    this.submittingForm = false;
+
+    if (error.status === 422){
+      this.serverErrorMessages = JSON.parse(error._body).errors;
+    } else {
+      this.serverErrorMessages = ['Falha na comunição com o servidor por favor tente mais tarde!'];
     }
   }
 
