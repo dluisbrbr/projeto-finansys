597410e7bdb4ebfa502157b5b1cc793f95fe2c05
dluisbrbr <dluisbr@yahoo.com.br>
2019-05-10 19:24:08 -0300

    refatoracao core e shred module 1

--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from "@angular/forms";
+import { SharedModule } from "../../shared/shared.module";
 
 import { EntriesRoutingModule } from './entries-routing.module';
-
-import { CalendarModule } from 'primeng/calendar'
-import { IMaskModule } from 'angular-imask'
 
 import { EntryListComponent } from "./entry-list/entry-list.component";
 import { EntryFormComponent } from "./entry-form/entry-form.component";
 
-
+import { CalendarModule } from 'primeng/calendar'
+import { IMaskModule } from 'angular-imask'
 
 @NgModule({
   declarations: [
@@ -18,9 +15,8 @@ import { EntryFormComponent } from "./entry-form/entry-form.component";
     EntryFormComponent
   ],
   imports: [
-    CommonModule,
+    SharedModule,
     EntriesRoutingModule,
-    ReactiveFormsModule,
     CalendarModule,
     IMaskModule
   ]
