5aa10a5e9d43a6231070a5b79a8d007ebb3535c8
dluisbrbr <dluisbr@yahoo.com.br>
2019-05-10 12:10:55 -0300

    entry service modificado para adicionar category class

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -5,6 +5,7 @@ import { Observable, throwError } from 'rxjs';
 import { map, catchError, flatMap } from 'rxjs/operators';
 
 import { Entry } from './entry.model';
+import { CategoryService } from "../../categories/shared/category.service";
 
 
 
@@ -16,7 +17,8 @@ export class EntryService {
   private apiPath: string = 'api/entries';
 
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+              private categoryService: CategoryService ) { }
 
   getAll(): Observable<Entry[]> {
     return this.http.get(this.apiPath).pipe(
@@ -35,18 +37,38 @@ export class EntryService {
   }
 
   create(entry: Entry): Observable<Entry>{
-    return this.http.post(this.apiPath, entry).pipe(
-      catchError(this.handlerError),
-      map(this.jsonDataToEntry)
+
+    // necessario para ficar compativel com o modelo do banco in-memory-web-api
+    return this.categoryService.getByID(entry.categoryId).pipe(
+      flatMap(
+        category => {
+          entry.category = category;
+
+          return this.http.post(this.apiPath, entry).pipe(
+            catchError(this.handlerError),
+            map(this.jsonDataToEntry)
+          )
+        }
+      )
     )
+
   }
 
   update(entry: Entry): Observable<Entry> { 
     const url = `${this.apiPath}/${entry.id}`;
 
-    return this.http.put(url, entry).pipe(
-      catchError(this.handlerError),
-      map(  () => entry )
+    // necessario para ficar compativel com o modelo do banco in-memory-web-api
+    return this.categoryService.getByID(entry.categoryId).pipe(
+      flatMap(
+        category => {
+          entry.category = category;
+
+          return this.http.put(url, entry).pipe(
+            catchError(this.handlerError),
+            map(() => entry)
+          )
+        }
+      )
     )
   }
 
